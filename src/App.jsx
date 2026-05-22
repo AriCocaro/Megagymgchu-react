6cea8ed3eeb559cf291de23abe40c10f7b78d262
AriCocaro <lic.aricocaro@gmail.com>
2026-05-22 12:32:52 +0000

    Se agrego pagina Tienda

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import Conocenos from './pages/Conocenos'
+import Tienda from './pages/Tienda'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/conocenos" element={<Conocenos />} />
+        <Route path= "/tienda" element={<Tienda />} />
       </Routes>
       <Footer />
     </BrowserRouter>
